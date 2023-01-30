82c97ea111faa5b7361ec535f64e7c676e39f169
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2023-01-30 20:39:25 +0000

    code refactoring, setup for prod deployment

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
     """view for the home page"""
+
     template_name = "home.html"
 
 
 class AboutPageView(TemplateView):
     """view for the about page"""
+
     template_name = "about.html"
